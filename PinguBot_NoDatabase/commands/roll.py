e0885a52857cc826daac231c4ef721031eeee3fa
JustAnyone <dom.svetikas@gmail.com>
2021-03-28 01:07:49 +0200

    Combine roll and roll6 cogs into one

--- PinguBot_NoDatabase/commands/roll.py
+++ PinguBot_NoDatabase/commands/roll.py
@@ -1,3 +1,5 @@
+import discord
+
 from discord.ext import commands
 from random import randint
 
@@ -10,6 +12,17 @@ class Roll(commands.Cog):
         if maxValue <= 0:
             maxValue = 1
         await ctx.send(f'You rolled a: {randint(1, maxValue)}')
+
+    @commands.command()
+    async def roll6(self, ctx):
+        rolled = randint(1, 6)
+        if rolled == 6:
+            await ctx.send(
+                'You\'re extra lucky today! Here have some gold coins:',
+                file=discord.File('./images/coin_pouch.gif')
+            )
+        await ctx.send(f'You rolled a: {rolled}')
+
 
 def setup(client):
     client.add_cog(Roll(client))
